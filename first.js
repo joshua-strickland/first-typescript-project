e4126084af4beccf74cabf98f5172c5a31d6c8e0
joshua-strickland <thejoshstrickland@gmail.com>
2021-01-03 16:03:48 -0500

    array testing

--- first.js
+++ first.js
@@ -1,3 +1,14 @@
 var catBreed;
 catBreed = "manx";
 console.log(catBreed);
+var list = [5, 2, 3];
+console.log(list[0]);
+for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
+    var i = list_1[_i];
+    console.log(i);
+}
+var stringList = ["asd", "asd2"];
+for (var _a = 0, stringList_1 = stringList; _a < stringList_1.length; _a++) {
+    var i = stringList_1[_a];
+    console.log(i);
+}
--- first.ts
+++ first.ts
@@ -3,3 +3,16 @@ var catBreed: String;
 catBreed = "manx";
 
 console.log(catBreed);
+
+let list: number[] = [5,2,3];
+
+console.log(list[0])
+
+for (let i of list){
+    console.log(i);
+}
+
+let stringList: string[] =["asd","asd2"];
+for (let i of stringList){
+    console.log(i);
+}
